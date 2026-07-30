c7cccd4a5b080a102b9f654cc5873495b4764095
Shagun <shagunnichant93@gmail.com>
2026-07-30 22:31:45 +0530

    Learned Substring

---
+++ 01-JavaScript/compareSliceSubstring.js
@@ -0,0 +1,4 @@
+let str = "Comparision";
+
+console.log(`Slice output: ${str.slice(-4)}`);
+console.log(`Substring output: ${str.substring(-4)}`);
---
+++ 01-JavaScript/exampleSubstring.js
@@ -0,0 +1,28 @@
+let fullName = "John Doe";
+console.log(`First Name: ${fullName.substring(0,4)}`);
+
+let productName = "LAPTOP-2025";
+console.log(`Product Code: ${productName.slice(0,6)}`);
+
+let patientId = "PAT458921";
+console.log(`Patient: ${patientId.substring(0,3)}`);
+
+let empId = "EMP10025";
+console.log(`Employee: ${empId.substring(0,3)}`);
+
+let cityName = "New Delhi";
+console.log(`City: ${cityName.substring(4,10)}`);
+
+let mailId = "john@gmail.com";
+console.log(`Extension: ${mailId.substring(5,14)}`);
+
+let movie = "SpiderMan";
+console.log(`Movie: ${movie.substring(0,6)}`);
+
+let bankName = "HDFC BANK";
+console.log(`Bank: ${bankName.substring(5,9)}`);
+
+let fileName = "resume.pdf";
+console.log(`Extension: ${fileName.substring(7,10)}`);
+
+
