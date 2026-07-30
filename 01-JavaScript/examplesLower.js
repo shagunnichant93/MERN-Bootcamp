c5bd2a869f94cd21b38c67c979b9faf93d765561
Shagun <shagunnichant93@gmail.com>
2026-07-30 15:31:50 +0530

    Learned toLowerCase

---
+++ 01-JavaScript/examplesLower.js
@@ -0,0 +1,26 @@
+let employeeName = "JOHN DOE";
+console.log("Employee Name: " + employeeName.toLowerCase());
+
+let bankName = "HDFC BANK";
+console.log("Bank Name: " + bankName.toLowerCase());
+
+let hospitalName = "APOLLO HOSPITAL";
+console.log("Hospital Name: " + hospitalName.toLowerCase());
+
+let studentName = "JANE SMITH";
+console.log("Student Name: " + studentName.toLowerCase());
+
+let restaurantName = "PIZZA HUT";
+console.log("Restaurant Name: " + restaurantName.toLowerCase());
+
+let userName = "MICHAEL JOHNSON";
+console.log("User Name: " + userName.toLowerCase());
+
+let emailAddress = "ABC@GMAIL.COM";
+console.log("Email Address: " + emailAddress.toLowerCase());
+
+let movieName = "INCEPTION";
+console.log("Movie Name: " + movieName.toLowerCase());
+
+let cityName = "NEW YORK";
+console.log("City Name: " + cityName.toLowerCase());
---
+++ 01-JavaScript/loginValidation.js
@@ -0,0 +1,5 @@
+let userName = "JOHN DOE";
+let emailAddress = "JOHN@GMAIL.COM";
+
+console.log("User Name: " + userName.toLowerCase());
+console.log("Email Address: " + emailAddress.toLowerCase());
