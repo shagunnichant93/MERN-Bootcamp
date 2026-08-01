a4236eae17970d76e8ff074b93ec23e085b5d8bd
Shagun <shagunnichant93@gmail.com>
2026-08-01 16:06:43 +0530

    Learned Split

---
+++ 01-JavaScript/emailAnalyzer.js
@@ -0,0 +1,7 @@
+let email = "john@gmail.com";   
+let parts = email.split("@");
+console.log(`Username : ${parts[0]}`);
+console.log(`Domain : ${parts[1]}`);
+
+
+console.log("John Doe".split(" "));
---
+++ 01-JavaScript/exampleSplit.js
@@ -0,0 +1,45 @@
+let name = "John Doe";
+console.log(`First Name : ${name.split(" ")[0]}`);
+console.log(`Last Name : ${name.split(" ")[1]}`);
+
+let email = "john@gmail.com";
+console.log(`Username : ${email.split("@")[0]}`);
+console.log(`Email Domain : ${email.split("@")[1]}`);       
+
+let productCode = "Laptop-2025";
+console.log(`Product Type : ${productCode.split("-")[0]}`);
+console.log(`Product Year : ${productCode.split("-")[1]}`);
+
+let date = "01/08/2026";
+console.log(`Day : ${date.split("/")[0]}`);
+console.log(`Month : ${date.split("/")[1]}`);
+console.log(`Year : ${date.split("/")[2]}`);
+
+let fileName = "resume.pdf";
+console.log(`File Name : ${fileName.split(".")[0]}`);
+console.log(`File Extension : ${fileName.split(".")[1]}`);
+
+let employeeId = "EMP-10025";
+console.log(`Employee Prefix : ${employeeId.split("-")[0]}`);
+console.log(`Employee Number : ${employeeId.split("-")[1]}`);
+
+let patientId = "PAT-458921";
+console.log(`Patient Prefix : ${patientId.split("-")[0]}`);
+console.log(`Patient Number : ${patientId.split("-")[1]}`);
+
+let bank = "ACC-123456";
+console.log(`Account Type : ${bank.split("-")[0]}`);
+console.log(`Bank Account Number : ${bank.split("-")[1]}`);
+
+let fruits = "Apple,Mango,Banana,Orange";
+console.log(`Fruit 1 : ${fruits.split(",")[0]}`);
+console.log(`Fruit 2 : ${fruits.split(",")[1]}`);
+console.log(`Fruit 3 : ${fruits.split(",")[2]}`);
+console.log(`Fruit 4 : ${fruits.split(",")[3]}`);
+
+let student = "John,Rahul,Shagun,Priya";
+console.log(`Student 1 : ${student.split(",")[0]}`);
+console.log(`Student 2 : ${student.split(",")[1]}`);
+console.log(`Student 3 : ${student.split(",")[2]}`);
+console.log(`Student 4 : ${student.split(",")[3]}`);
+
