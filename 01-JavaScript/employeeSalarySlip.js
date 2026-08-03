1cf9c37892b3576727f97d070514ab4d9c227b7d
Shagun <shagunnichant93@gmail.com>
2026-08-03 22:19:36 +0530

    Learned TemplateLiterals

---
+++ 01-JavaScript/employeeSalarySlip.js
@@ -0,0 +1,7 @@
+let employee = "John";
+let department = "IT";
+let salary = 75000;
+
+console.log(`Employee Name: ${employee}`);
+console.log(`Department: ${department}`);
+console.log(`Salary: $${salary}`);
---
+++ 01-JavaScript/exampleLiterals.js
@@ -0,0 +1,46 @@
+let employee = "John";
+let salary = 70000;
+console.log(`Employee Name: ${employee}`);
+console.log(`Salary: ${salary}`);
+
+let student = "Rahul";
+let course = "MERN Stack";
+console.log(`Student Name: ${student}`);
+console.log(`Course: ${course}`);
+
+let account = "ACC1234";    
+let bal = 25000;
+console.log(`Account Number: ${account}`);
+console.log(`Balance: ${bal}`);
+
+let patient = "Amit";
+let disease = "Fever";
+console.log(`Patient Name: ${patient}`);
+console.log(`Disease: ${disease}`);
+
+let product = "Laptop"; 
+let price = 65000;
+console.log(`Product Name: ${product}`);
+console.log(`Price: ${price}`);
+
+let food = "Burger";
+let quantity = 3;
+console.log(`You ordered ${quantity} ${food}(s)`);
+
+let movie = "Inception";
+let seats = 4;
+console.log(`Movie: ${movie}`);
+console.log(`Seats Booked: ${seats}`);
+
+let passenger = "Priya";
+let destination = "Goa";
+console.log(`Passenger Name: ${passenger}`);
+console.log(`Destination: ${destination}`);
+
+let customer = "Mohan";
+let bill = 1800;
+console.log(`Customer Name: ${customer}`);
+console.log(`Bill Amount: ${bill}`);
+
+let user = "Rahul";
+console.log(`Welcome ${user}!`);
