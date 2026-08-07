d92a4bef3eb91a31e31056d4ba8e564db8e41d2c
Shagun <shagunnichant93@gmail.com>
2026-08-07 17:45:01 +0530

    Learned Array Reverse()

---
+++ 01-JavaScript/bankTransactionReverse.js
@@ -0,0 +1,21 @@
+let transactions = [
+"Deposit",
+"Withdraw",
+"Transfer",
+"Recharge",
+"Loan EMI"
+];
+console.log(`Original Transactions:
+    ${transactions[0]}
+    ${transactions[1]}
+    ${transactions[2]}
+    ${transactions[3]}
+    ${transactions[4]}`);
+console.log(`-----------------------------`);
+let rev = transactions.reverse();
+console.log(`Recent Transactions:
+    ${transactions[0]}
+    ${transactions[1]}
+    ${transactions[2]}
+    ${transactions[3]}
+    ${transactions[4]}`)
---
+++ 01-JavaScript/exampleReverse.js
@@ -0,0 +1,41 @@
+let fruits =  ["Apple", "Banana","Mango"];
+console.log(`Fruits: ${fruits}`);
+let fruitReverse = fruits.reverse();
+console.log(`Fruits after reversing: ${fruitReverse}`);
+
+let students = ["Jake","Jack","Jill","John"];
+console.log(`Students: ${students}`);
+let studentReverse = students.reverse();
+console.log(`Students after reversing: ${studentReverse}`);
+
+let cities =  ["Delhi","New York", "Los Angeles", "Chicago", "Houston"];
+console.log(`cities`);
+let citiesReverse = cities.reverse();
+console.log(`Cities after reversing: ${citiesReverse}`);
+
+let products = ["Laptop", "Monitor", "Tablet", "Smartphone"];
+console.log(`Products: ${products}`);
+let productsReverse = products.reverse();
+console.log(`Products after reversing: ${productsReverse}`);
+
+let movies = ["Inception", "Spiderman", "The Dark Knight", "Interstellar", "Avengers"];
+console.log(`Movies:${movies}`);
+let movieReverse = movies.reverse();
+console.log(`Movies after reversing: ${movieReverse}`);
+
+let bankaccounts = ["Savings", "Checking", "Investment", "Current"];
+console.log(`Bank Accounts:${bankaccounts}`);
+console.log(`Accounts after reversing: ${bankaccounts.reverse()}`);
+
+let numbers = [1,2,3,4,5];
+console.log(`Numbers: ${numbers}`);
+console.log(`Numbers after reversing: ${numbers.reverse()}`);
+console.log(`Arrays are mutable`);
+
+let arr =["A"];
+console.log(`Array: ${arr}`);
+console.log(`Array after reversing: ${arr.reverse()}`);
+
+let emptyArray = [];
+console.log(`Empty Array: ${emptyArray}`);
+console.log(`Empty Array after reversing: ${emptyArray.reverse()}`);
